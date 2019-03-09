e862cb9894cf031961b7a35df8946bd3b0845d7e
Peter Varney <pvarney@vshed.us>
2019-03-09 16:03:08 -0500

    Initial Release

---
+++ src/UNC Share/UNC Share Test/UsageTests.cs
@@ -0,0 +1,98 @@
+﻿/* 
+ *Copyright (C) 2019 Peter Varney - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, 
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, visit : https://github.com/fatalwall/vshed.IO.UncShare
+ */
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using vshed.IO;
+using System.IO;
+
+namespace UNC_Share_Test
+{
+    [TestClass]
+    public class UsageTests
+    {
+        [TestMethod]
+        public void Connection_Disconnect()
+        {
+            try
+            {
+                using (var share = new UncShare(@"\\nas01\BatMonTestFolder", "usrBatMonTest", "*=%$p$")) {}
+            }
+            catch (Exception unknown) { Assert.Fail(ExceptionFailMessage(unknown)); }
+        }
+
+        [TestMethod]
+        public void EnumerateFiles()
+        {
+            try
+            {
+                using (var share = new UncShare(@"\\nas01\BatMonTestFolder", "usrBatMonTest", "*=%$p$"))
+                {
+                    var dir = new DirectoryInfo(share.Path);
+                    Assert.IsTrue(dir.GetFiles().Length >= 1);
+                }
+            }
+            catch (Exception unknown) { Assert.Fail(ExceptionFailMessage(unknown)); }
+        }
+
+        [TestMethod]
+        public void Invalid_Path_ErrorHandling()
+        {
+            try
+            {
+                using (var share = new UncShare(@"\\nas01\NonExistantFolder", "usrBatMonTest", "*=%$p$")) { }
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                if (ex.Message == "The network path was not found")
+                    Assert.IsTrue(true);
+                else Assert.Fail(ExceptionFailMessage(ex));
+            }
+            catch (Exception unknown) { Assert.Fail(ExceptionFailMessage(unknown)); }
+        }
+
+        [TestMethod]
+        public void Invalid_User_ErrorHandling()
+        {
+            try
+            {
+                using (var share = new UncShare(@"\\nas01\BatMonTestFolder", "wrongUser", "*=%$p$")) { }
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                if (ex.Message == "The user name or password is incorrect")
+                    Assert.IsTrue(true);
+                else Assert.Fail(ExceptionFailMessage(ex));
+            }
+            catch (Exception unknown) { Assert.Fail(ExceptionFailMessage(unknown)); }
+        }
+
+        [TestMethod]
+        public void Invalid_Password_ErrorHandling()
+        {
+            try
+            {
+                using (var share = new UncShare(@"\\nas01\BatMonTestFolder", "usrBatMonTest", "wrongPassword")) { }
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                if (ex.Message == "The user name or password is incorrect")
+                    Assert.IsTrue(true);
+                else Assert.Fail(ExceptionFailMessage(ex));
+            }
+            catch (Exception unknown) { Assert.Fail(ExceptionFailMessage(unknown)); }
+        }
+
+        private string ExceptionFailMessage(Exception ex)
+        {
+            return "Excetpion was thrown of type:" + ex.GetType().ToString() + " with message:" + ex.Message;
+        }
+
+
+    }
+}
---
+++ src/UNC Share/UNC Share/UncShare.cs
@@ -0,0 +1,62 @@
+﻿/* 
+ *Copyright (C) 2019 Peter Varney - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, 
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, visit : https://github.com/fatalwall/System.IO.UncShare
+ */
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace vshed.IO
+{
+    public class UncShare : IDisposable
+    {
+        public string Path { get; private set; }
+
+        public UncShare(string uncPath, string userName, string password)
+        {
+            int result = WNetUseConnection(IntPtr.Zero
+                                            , new NETRESOURCE { dwType = 0x00000001, lpRemoteName = uncPath }
+                                            , password
+                                            , userName
+                                            , 0
+                                            , null
+                                            , null
+                                            , null);
+            if(result != 0) { throw new Win32Exception(result); }
+            this.Path = uncPath;
+        }
+
+        public void Dispose()
+        {
+            if (!string.IsNullOrWhiteSpace(this.Path))
+            {
+                WNetCancelConnection2(this.Path, 0x00000001, false);
+                this.Path = null;
+            }
+        }
+
+        [DllImport("mpr.dll")]
+        private static extern int WNetUseConnection(IntPtr hwndOwner, NETRESOURCE lpNetResource, string lpPassword, string lpUserID, int dwFlags, string lpAccessname, string lpBufferSize, string lpResult);
+
+        [DllImport("mpr.dll")]
+        private static extern int WNetCancelConnection2(string lpName, int dwFlags, bool fForce);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private class NETRESOURCE
+        {
+            public int dwScope;
+            public int dwType;
+            public int dwDisplayType;
+            public int dwUsage;
+            public string lpLocalName;
+            public string lpRemoteName;
+            public string lpComment;
+            public string lpProvider;
+
+        }
+    }
+}
